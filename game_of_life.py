56bc88169ac4ee9947f5e717f41360d6f9b31292
elizabeth <toelizabethes@gmail.com>
2025-01-17 01:07:35 +0300

    Changed init method of the GameOfLife cs

--- game_of_life.py
+++ game_of_life.py
@@ -4,21 +4,22 @@ import sys
 GRAY = (177, 177, 177)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-FPS = 20
-block_size = 10
-WINDOW_HEIGHT = 800
-WINDOW_WIDTH = 800
+
 
 
 class GameOfLife:
-    def __init__(self):
+    def __init__(self, WINDOW_HEIGHT, WINDOW_WIDTH, BLOCK_SIZE, FPS):
+        self.WINDOW_HEIGHT = WINDOW_HEIGHT
+        self.WINDOW_WIDTH = WINDOW_WIDTH
+        self.block_size = BLOCK_SIZE
+        self.FPS = FPS
         pygame.init()
         pygame.font.init()
         pygame.display.set_caption('Life')
         self.SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.SCREEN.fill(WHITE)
         self.clock = pygame.time.Clock()
-        self.board = [[0 for _ in range(int(WINDOW_HEIGHT / block_size))] for _ in range(int(WINDOW_WIDTH / block_size))]
+        self.board = [[0 for _ in range(int(WINDOW_HEIGHT / self.block_size))] for _ in range(int(WINDOW_WIDTH / self.block_size))]
         self.draw_grid()
         self.drawing_screen()
         self.game()
@@ -55,24 +56,24 @@ class GameOfLife:
 
     def draw_grid(self):
         self.SCREEN.fill(WHITE)
-        for x in range(0, WINDOW_WIDTH, block_size):
-            for y in range(0, WINDOW_HEIGHT, block_size):
-                rect = pygame.Rect(x, y, block_size, block_size)
+        for x in range(0, self.WINDOW_WIDTH, self.block_size):
+            for y in range(0, self.WINDOW_HEIGHT, self.block_size):
+                rect = pygame.Rect(x, y, self.block_size, self.block_size)
                 pygame.draw.rect(self.SCREEN, GRAY, rect, 1)
         self.draw_all_cells()
 
     def draw_cell(self, mouseposition):
-        x = mouseposition[0] // block_size
-        y = mouseposition[1] // block_size
-        if (mouseposition[1] < WINDOW_HEIGHT and mouseposition[0] < WINDOW_WIDTH):
+        x = mouseposition[0] // self.block_size
+        y = mouseposition[1] // self.block_size
+        if (mouseposition[1] < self.WINDOW_HEIGHT and mouseposition[0] < self.WINDOW_WIDTH):
             if self.board[x][y] == 0:
                 self.board[x][y] = 1
-                rectangle = pygame.Rect(x * block_size, y * block_size, block_size, block_size)
+                rectangle = pygame.Rect(x * self.block_size, y * self.block_size, self.block_size, self.block_size)
                 pygame.draw.rect(self.SCREEN, BLACK, rectangle)
 
             else:
                 self.board[x][y] = 0
-                rectangle = pygame.Rect(x * block_size, y * block_size, block_size, block_size)
+                rectangle = pygame.Rect(x * self.block_size, y * self.block_size, self.block_size, self.block_size)
                 pygame.draw.rect(self.SCREEN, WHITE, rectangle)
         self.draw_grid()
 
@@ -80,14 +81,14 @@ class GameOfLife:
         for i in range(len(self.board)):
             for j in range(len(self.board[0])):
                 if self.board[i][j] == 1:
-                    rectangle = pygame.Rect(i * block_size, j * block_size, block_size, block_size)
+                    rectangle = pygame.Rect(i * self.block_size, j * self.block_size, self.block_size, self.block_size)
                     pygame.draw.rect(self.SCREEN, BLACK, rectangle)
 
     def drawing_screen(self):
         loop = True
         font = pygame.font.SysFont('bahnschrift', 40)
         label = font.render("Press SPACE to start after drawing the pattern", 1, (0, 0, 0))
-        text_rect = label.get_rect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+        text_rect = label.get_rect(center=(self.WINDOW_WIDTH / 2, self.WINDOW_HEIGHT / 2))
         self.SCREEN.blit(label, text_rect)
         while loop:
             for event in pygame.event.get():
@@ -107,7 +108,7 @@ class GameOfLife:
     def game(self):
         running = True
         while running:
-            self.clock.tick(FPS)
+            self.clock.tick(self.FPS)
             self.check()
             self.draw_grid()
             pygame.display.update()
@@ -123,5 +124,5 @@ class GameOfLife:
             pygame.display.update()
 
 if __name__ == "__main__":
-    Game = GameOfLife()
+    Game = GameOfLife(800, 800, 10, 20)
     
